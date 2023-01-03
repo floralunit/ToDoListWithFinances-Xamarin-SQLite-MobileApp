6ccabaa85f4c992531614e14a11464ea415ddc30
floralunit <71765648+floralunit@users.noreply.github.com>
2023-01-04 00:22:11 +0300

    создан проект по шаблону

---
+++ FloralMobileApp/FloralMobileApp/FloralMobileApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FloralMobileApp.Services;
+using FloralMobileApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FloralMobileApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FloralMobileApp/FloralMobileApp/FloralMobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FloralMobileApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FloralMobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
