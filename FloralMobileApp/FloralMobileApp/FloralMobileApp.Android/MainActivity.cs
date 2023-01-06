06e313d01664de2a9e80b12627898c6bcbfa7fb0
floralunit <71765648+floralunit@users.noreply.github.com>
2023-01-07 00:19:27 +0300

    сделан основной дизайн и мелкие правки

--- FloralMobileApp/FloralMobileApp/FloralMobileApp.Android/MainActivity.cs
+++ FloralMobileApp/FloralMobileApp/FloralMobileApp.Android/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 using Plugin.CurrentActivity;
+using ImageCircle.Forms.Plugin.Droid;
 
 namespace FloralMobileApp.Droid
 {
@@ -18,6 +19,7 @@ namespace FloralMobileApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            ImageCircleRenderer.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ FloralMobileApp/FloralMobileApp/FloralMobileApp/Views/HeaderContentView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FloralMobileApp.Views
+{
+    public partial class HeaderContentView : ContentView
+    {
+        public HeaderContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
