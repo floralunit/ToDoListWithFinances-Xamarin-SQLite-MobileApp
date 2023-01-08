9762047cd5a710ece135ad79483bc83f7dacd72c
floralunit <71765648+floralunit@users.noreply.github.com>
2023-01-08 22:25:03 +0300

    доделана страница "о программе"

--- FloralMobileApp/FloralMobileApp/FloralMobileApp/ViewModels/AboutViewModel.cs
+++ FloralMobileApp/FloralMobileApp/FloralMobileApp/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace FloralMobileApp.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "ስውር አካል በተለያዩ የኢሶቴሪክ";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
